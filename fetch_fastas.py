8f03a80ddbc91250a826d7e0b27d4f3fd47184b3
kcamnairb <kcamnairb@yahoo.com>
2017-01-28 15:34:52 -0600

    Changed output to stdout instead of a file.

--- fetch_fastas.py
+++ fetch_fastas.py
@@ -4,6 +4,7 @@ from os import listdir
 from os.path import isfile, join
 from Bio import AlignIO, SeqIO
 from io import StringIO
+
 parser = argparse.ArgumentParser(description="Retrieves individual fastas from multifasta files. Each fasta ID that you want to fetch should be on its own line.")
 parser.add_argument("fastas_to_fetch")
 parser.add_argument("fasta_files", nargs='+')
@@ -13,16 +14,15 @@ fasta_dict = {}
 for file in args.fasta_files:
     fasta_dict.update(SeqIO.to_dict(SeqIO.parse(open(file),"fasta")))
 with open(args.fastas_to_fetch) if args.fastas_to_fetch is not "-" else sys.stdin as f:
-	target_fastas = [fasta.strip().split(" ")[0] for fasta in f]
+    target_fastas = [fasta.strip().split(" ")[0] for fasta in f]
 num_fastas_fetched = 0
 missing = []
-with open("fetched_fastas.fasta","w") as outfile:
-	for fasta in target_fastas:
-		if fasta in fasta_dict:
-			SeqIO.write(fasta_dict[fasta],outfile,"fasta")
-			num_fastas_fetched += 1
-		else:
-			missing.append(fasta)
-print(str(num_fastas_fetched) + ' fastas fetched')
+for fasta in target_fastas:
+    if fasta in fasta_dict:
+        SeqIO.write(fasta_dict[fasta],sys.stdout,"fasta")
+        num_fastas_fetched += 1
+    else:
+        missing.append(fasta)
+sys.stderr.write(str(num_fastas_fetched) + ' fastas fetched\n')
 if missing:
-	print('Could not find ' + str(', '.join(missing)))
+    sys.stderr.write('Could not find ' + str(', '.join(missing))+'\n')
